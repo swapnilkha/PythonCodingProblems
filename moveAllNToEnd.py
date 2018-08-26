3e20ade2a9b57b749e2dfe6cf90bc83857c2bf81
Swapnil Kha <swapnilkha@gmail.com>
2018-08-26 14:12:40 -0400

    Create moveAllNToEnd.py

---
+++ moveAllNToEnd.py
@@ -0,0 +1,13 @@
+*Given a list, move all n's to the end*
+
+def moveAllNToEnd(lst, n):
+    pos = 0
+    for i in range(len(lst)):
+        if lst[i] != n:
+            lst[pos] = lst[i]
+            pos += 1
+    while pos < len(lst):
+        lst[pos] = n
+        pos += 1
+    return lst
+   
